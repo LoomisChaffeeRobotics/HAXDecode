bb73394386a9e9edee41db4a7ffe56c7674d060b
Gavintool <Gavintool@163.com>
2025-09-16 22:13:52 -0400

    Added code for revolver + colorsort

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PID {
+        public static double PID_P = 0;
+        public static double PID_I = 0;
+        public static double PID_D = 0;
+        public double velo = 0;
+        public double pidInt = 0;
+        public double pidDev = 0;
+        public double errorCur = 0;
+        public double errorPrev = 0;
+        public double iMax;
+        public double iRange;
+        public double Kp;
+        public double Ki;
+        public double Kd;
+        public double target;
+        public boolean arrived;
+        public double errorTol;
+
+        public void PID(){
+
+        }
+
+        public void setCoefficients(double p, double i, double d) {
+            Kp = p;
+            Ki = i;
+            Kd = d;
+        }
+        public void init() {
+            pidInt = 0;
+            pidDev = 0;
+            errorPrev = target;
+        }
+
+        public void update(double current) {
+            errorCur = target - current;
+            PID_P = errorCur * Kp;
+
+            pidDev = errorCur - errorPrev;
+            errorPrev = errorCur;
+            PID_D = pidDev * Kd;
+
+            if (Math.abs(PID_P) >= iRange) {
+                pidInt = 0;
+            } else {
+                pidInt += errorCur;
+            }
+
+            if (Math.abs(pidInt * Ki) >= iMax) {
+                pidInt = Math.signum(pidInt) * iMax / Ki;
+            }
+            if ((Math.signum(errorCur) != Math.signum(pidInt)) || (Math.abs(errorCur) < errorTol)) {
+                pidInt = 0;
+            }
+            PID_I = pidInt * Ki;
+
+            velo = PID_P + PID_I + PID_D;
+        }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Revolver.java
@@ -0,0 +1,144 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+@TeleOp
+public class Revolver extends OpMode{
+    //-----------------functions-------------
+    int sign(double num){
+        if (num > 0){
+            return 1;
+        }
+        else if (num < 0){
+            return -1;
+        }
+        return 0;
+    }
+    void detectColor() {
+        colors = colorSens.getNormalizedColors();
+        Color.colorToHSV(colors.toColor(), hsvValues);
+        hue = hsvValues[0];
+        if (hue > purpleMin && hue < purpleMax){
+            curColor = "purple";
+        }
+        else if (hue > greenMin && hue < greenMax){
+            curColor = "green";
+        }
+        else {
+            curColor = "white";
+        }
+    }
+    void shiftTarget(double times){
+        double ticks = fullCircle * times;
+        slotTarget[0] += ticks;
+        slotTarget[1] += ticks;
+        slotTarget[2] += ticks;
+    }
+    double calcTimes(){
+        if (sign(curPos) == 1){
+            return Math.floor(curPos / fullCircle);
+        }
+        else if (sign(curPos) == -1){
+            return Math.ceil(curPos / fullCircle);
+        }
+        return 0;
+    }
+    //---------------------Define stuff------------------
+    DcMotor revSpin;
+    NormalizedColorSensor colorSens;
+    NormalizedRGBA colors;
+    PID pid = new PID();
+    //--------------------------variables------------------
+    float gain = 1;
+    boolean UPPRESSED = false;
+    boolean DOWNPRESSED = false;
+    boolean APRESSED = false;
+    double hue;
+    int pointer = 0;
+    double curPos = 0;
+    double fullCircle = 1400;
+    String curColor = "white";
+    final float[] hsvValues = new float[3];
+    double[] slotTarget = {0, 120, 240};
+    //-----------define colors--------------
+    float greenMin = 0;
+    float greenMax = 0;
+    float purpleMin = 0;
+    float purpleMax = 0;
+    float target = 0;
+    //------------------------------init---------------------------------
+    @Override
+    public void init() {
+        pid.init();
+        pid.setCoefficients(0, 0, 0);
+        pid.iMax = 0.3;
+        pid.iRange = 0.5;
+        pid.errorTol = 4;
+        pid.target = target;
+        colorSens = hardwareMap.get(NormalizedColorSensor.class, "color");
+        revSpin = hardwareMap.get(DcMotor.class, "Spin");
+        revSpin.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        revSpin.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        colorSens.setGain(gain);
+        if (colorSens instanceof SwitchableLight) {
+            ((SwitchableLight)colorSens).enableLight(true);
+        }
+    }
+    //-------------------------------loop--------------------------------
+    @Override
+    public void loop() {
+        //---------------Adjust target values------------
+        curPos = revSpin.getCurrentPosition();
+        shiftTarget(calcTimes());
+        //--------------------change gain per loop-------------------------
+        if (gamepad1.dpad_up && !UPPRESSED){
+            gain += 0.05;
+            colorSens.setGain(gain);
+        }
+        else if (gamepad1.dpad_down && !DOWNPRESSED){
+            gain -= 0.05;
+            colorSens.setGain(gain);
+        }
+        //-----------------------toggle lights-----------------------
+        if (gamepad1.a && !APRESSED){
+            if (colorSens instanceof SwitchableLight) {
+                SwitchableLight light = (SwitchableLight)colorSens;
+                light.enableLight(!light.isLightOn());
+            }
+        }
+        //---------------------spin revolver---------------------------
+        if (gamepad1.dpad_left){
+            pointer = (pointer + 1) % 3;
+        }
+        else if (gamepad1.dpad_right){
+            pointer = (pointer - 1) % 3;
+        }
+        //-------------------------------end cycle---------------------
+        if ( Math.abs(curPos - slotTarget[pointer]) > (fullCircle / 2) ) {
+            pid.target = sign(slotTarget[pointer]) * fullCircle + slotTarget[pointer];
+        }
+
+        pid.update(curPos);
+        revSpin.setPower(pid.velo);
+
+        APRESSED = gamepad1.a;
+        UPPRESSED = gamepad1.dpad_up;
+        DOWNPRESSED = gamepad1.dpad_down;
+
+        detectColor();
+
+        telemetry.addData("gain", gain);
+        telemetry.addData("hue", hue);
+        telemetry.addData("color", curColor);
+        telemetry.addData("pointer", pointer);
+        telemetry.addData("target", target);
+        telemetry.update();
+    }
+}
