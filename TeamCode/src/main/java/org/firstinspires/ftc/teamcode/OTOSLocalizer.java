77de92d7557196dfa86e8f567e5e954f1450af0d
soggy-cereal1 <polargalaxy.314@gmail.com>
2025-10-06 17:08:45 -0400

    started tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSLocalizer.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Config
 public class OTOSLocalizer implements Localizer {
     public static class Params {
-        public double angularScalar = 1.0;
+        public double angularScalar = -0.9927;
         public double linearScalar = 1.0;
 
         // Note: units are in inches and radians
