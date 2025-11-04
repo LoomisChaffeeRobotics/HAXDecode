6505aabfa1bdacca1cf21a677562e8e45f15e902
justin9413 <hyeonho0413@gmail.com>
2025-11-04 17:14:00 -0500

    Create Turret Speed Evalutor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretSpeedEvalutor.java
@@ -0,0 +1,119 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp
+@Config
+public class TurretSpeedEvalutor extends OpMode {
+    DcMotorEx innerTurret;
+    DcMotorEx outerTurret;
+    limelighttesting limelight;
+    FtcDashboard Dash=FtcDashboard.getInstance();
+    Telemetry t2=Dash.getTelemetry();
+
+    double currentlocationx = 0;
+    double rawdistancex = 0;
+    double currentlocationy = 0;
+    double rawdistancey = 0;
+    double rawDistance_inch = 0;
+    double rawDistance_meters = 0;
+    double currentvelocity_x = 0;
+    double currentvelocity_y = 0;
+    double velocitymagnitude = 0;
+    double costheta = 0;
+    double abs_distance;
+    double tof = 0;
+    double distanceUpdatedAfterVector_inch = 0;
+    double distanceUpdatedAfterVector_meter = 0;
+    double projectionMagnitude_inch = 0;
+    double upperTurretSpeed = 0;
+    double lowerTurretSpeed = 0;
+
+    @Override
+    public void init() {
+        limelight = new limelighttesting();
+        limelight.init(); //i forgot how to do it emam can you help  it's gonna give you an error probably
+        innerTurret=hardwareMap.get(DcMotorEx.class, "innerTurret");
+        outerTurret=hardwareMap.get(DcMotorEx.class, "outerTurret");
+
+        innerTurret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        outerTurret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        innerTurret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        outerTurret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        innerTurret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        outerTurret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    public double timeOfFlight (double distance){
+        abs_distance = Math.abs(distance);
+        if(abs_distance<=1.00) return 0.2*abs_distance+0.5;
+        else if(1.00<abs_distance && abs_distance<=1.50) return 0.16*abs_distance+0.54;
+        else if(1.50<abs_distance && abs_distance<=2.00) return 0.14*abs_distance+0.57;
+        else if(2.00<abs_distance && abs_distance<=2.50) return 0.16*abs_distance+0.53;
+        else if(2.50<abs_distance && abs_distance<=3.00) return 0.18*abs_distance+0.48;
+        else if(3.00<abs_distance && abs_distance<=3.50) return 0.14*abs_distance+0.6;
+        else return 0.2*abs_distance+0.39;
+    }
+    public double findUpperSpeed (double distance){
+        abs_distance = Math.abs(distance);
+        if(abs_distance<=1.00) return 36*abs_distance+2353;
+        else if(1.00<abs_distance && abs_distance<=1.50) return 374*abs_distance+2015;
+        else if(1.50<abs_distance && abs_distance<=2.00) return 596*abs_distance+1682;
+        else if(2.00<abs_distance && abs_distance<=2.50) return 738*abs_distance+1398;
+        else if(2.50<abs_distance && abs_distance<=3.00) return 912*abs_distance+963;
+        else if(3.00<abs_distance && abs_distance<=3.50) return 1134*abs_distance+209;
+        else return 0.2*abs_distance+0.39;
+    }
+    public double findLowerSpeed (double distance){
+        abs_distance = Math.abs(distance);
+        if(abs_distance<=1.00) return 1080*abs_distance+1575;
+        else if(1.00<abs_distance && abs_distance<=1.50) return 1006*abs_distance+1649;
+        else if(1.50<abs_distance && abs_distance<=2.00) return 1014*abs_distance+1637;
+        else if(2.00<abs_distance && abs_distance<=2.50) return 938*abs_distance+1789;
+        else if(2.50<abs_distance && abs_distance<=3.00) return 932*abs_distance+1804;
+        else if(3.00<abs_distance && abs_distance<=3.50) return 1044*abs_distance+1468;
+        else return 1074*abs_distance+1363;
+    }
+    @Override
+    public void loop() {
+        currentlocationx = limelight.botpose.getPosition().x;
+        currentlocationy = limelight.botpose.getPosition().y;
+        rawdistancex = (-58.346457) - currentlocationx;
+        rawdistancey = (-55.629921) - currentlocationy;
+
+        //get distance to goal
+        rawDistance_inch=Math.sqrt(rawdistancex*rawdistancex + rawdistancey*rawdistancey);
+        rawDistance_meters=rawDistance_inch*2.54/100;
+
+        currentvelocity_x = //somehow get x-velocity from roadrunner
+        currentvelocity_y = //somehow get y-velocity from roadrunner
+        velocitymagnitude = Math.sqrt(currentvelocity_x*currentvelocity_x + currentvelocity_y*currentvelocity_y);
+
+        //get cos theta of angle between two vectors;
+        costheta = ((currentlocationx * currentvelocity_x) + (currentlocationy * currentvelocity_y)) / (rawDistance_inch * velocitymagnitude);
+
+        //evaluate projection
+        projectionMagnitude_inch = velocitymagnitude * costheta;
+
+        //find time of flight
+        tof = timeOfFlight(rawDistance_meters);
+
+        distanceUpdatedAfterVector_inch = rawDistance_inch - projectionMagnitude_inch*tof;
+        distanceUpdatedAfterVector_meter = distanceUpdatedAfterVector_inch*2.54/100;
+        //evaluate turret speed
+        upperTurretSpeed = findUpperSpeed(distanceUpdatedAfterVector_meter);
+        lowerTurretSpeed = findLowerSpeed(distanceUpdatedAfterVector_meter);
+
+        outerTurret.setVelocity(upperTurretSpeed);
+        innerTurret.setVelocity(lowerTurretSpeed);
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelighttesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelighttesting.java
@@ -6,13 +6,20 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
 import java.util.List;
 
 @TeleOp
 public class limelighttesting extends OpMode {
+    public double tx;
+    public double ty;
+    public double ta;
+    public Pose3D botpose;
     Limelight3A limelight;
     @Override
     public void init() {
+
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
@@ -21,9 +28,10 @@ public class limelighttesting extends OpMode {
     public void loop() {
         LLResult result = limelight.getLatestResult();
         if (result != null && result.isValid()) {
-            double tx = result.getTx(); // How far left or right the target is (degrees)
-            double ty = result.getTy(); // How far up or down the target is (degrees)
-            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+            botpose = result.getBotpose();
+            tx = result.getTx(); // How far left or right the target is (degrees)
+            ty = result.getTy(); // How far up or down the target is (degrees)
+            ta = result.getTa(); // How big the target looks (0%-100% of the image)
             telemetry.addData("Target X", tx);
             telemetry.addData("Target Y", ty);
             telemetry.addData("Target Area", ta);
